fe374b4f42adb0d0e1d90dd29a7d77c127ded5ed
Sotos Zarpas <sotiriszarpas@hotmail.gr>
2025-02-26 20:29:26 +0100

    Added Category model that communicates with external service and cleaned the dto category class from attributes

---
+++ src/CSharpApp.Core/Models/CategoryServiceModel.cs
@@ -0,0 +1,20 @@
+﻿namespace CSharpApp.Core.Models
+{
+    public class CategoryServiceModel
+    {
+        [JsonPropertyName("id")]
+        public int? Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+
+        [JsonPropertyName("image")]
+        public string? Image { get; set; }
+
+        [JsonPropertyName("creationAt")]
+        public DateTime? CreationAt { get; set; }
+
+        [JsonPropertyName("updatedAt")]
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
