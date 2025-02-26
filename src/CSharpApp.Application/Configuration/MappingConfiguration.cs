256e215d71f3945fb498aa6dea2ba2a79228f042
Sotos Zarpas <sotiriszarpas@hotmail.gr>
2025-02-26 20:39:16 +0100

    Added custom mapper for category

--- src/CSharpApp.Application/Configuration/MappingConfiguration.cs
+++ src/CSharpApp.Application/Configuration/MappingConfiguration.cs
@@ -10,6 +10,8 @@ namespace CSharpApp.Application.Configuration
         {
             services.AddTransient<IMapper<ProductServiceModel, Product>, CustomProductMapper>();
             services.AddTransient<IMapper<Product, ProductServiceModel>, CustomProductMapper>();
+            services.AddTransient<IMapper<CategoryServiceModel, Category>, CustomCategoryMapper>();
+            services.AddTransient<IMapper<Category, CategoryServiceModel>, CustomCategoryMapper>();
             return services;
         }
     }
---
+++ src/CSharpApp.Application/Mappings/CustomCategoryMapper.cs
@@ -0,0 +1,53 @@
+﻿using CSharpApp.Core.Models;
+
+namespace CSharpApp.Application.Mappings
+{
+    public class CustomCategoryMapper : IMapper<CategoryServiceModel, Category>, IMapper<Category, CategoryServiceModel>
+    {
+        public Category Map(CategoryServiceModel source)
+        {
+            return new Category
+            {
+                Id = source.Id,
+                Name = source.Name,
+                Image = source.Image,
+                CreationAt = source.CreationAt,
+                UpdatedAt = source.UpdatedAt,
+            };
+        }
+
+        public List<Category> Map(IReadOnlyCollection<CategoryServiceModel> source)
+        {
+            var categories = new List<Category>();
+            foreach (var item in source)
+            {
+                categories.Add(new Category
+                {
+                    Id = item.Id,
+                    Name = item.Name,
+                    Image = item.Image,
+                    CreationAt = item.CreationAt,
+                    UpdatedAt = item.UpdatedAt,
+                });
+            }
+            return categories;
+        }
+
+        public CategoryServiceModel Map(Category source)
+        {
+            return new CategoryServiceModel
+            {
+                Id = source.Id,
+                Name = source.Name,
+                Image = source.Image,
+                CreationAt = source.CreationAt,
+                UpdatedAt = source.UpdatedAt,
+            };
+        }
+
+        public List<CategoryServiceModel> Map(IReadOnlyCollection<Category> source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
