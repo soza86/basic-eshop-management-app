14069fc8135a97a1ec6edfd86cc3e2652ef558e5
Sotos Zarpas <sotiriszarpas@hotmail.gr>
2025-02-27 06:57:26 +0100

    Added tests for product endpoints, changed Program class in API project to include top level statement

--- src/CSharpApp.Api/Program.cs
+++ src/CSharpApp.Api/Program.cs
@@ -6,85 +6,93 @@ using CSharpApp.Application.Configuration;
 using CSharpApp.Application.Queries.Category;
 using CSharpApp.Application.Queries.Product;
 using CSharpApp.Core.Dtos;
-using CSharpApp.Infrastructure.ExternalServices;
 using MediatR;
 
-var builder = WebApplication.CreateBuilder(args);
-
-var logger = new LoggerConfiguration().ReadFrom.Configuration(builder.Configuration).CreateLogger();
-builder.Logging.ClearProviders().AddSerilog(logger);
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-builder.Services.AddDefaultConfiguration();
-builder.Services.AddHttpConfiguration();
-builder.Services.AddProblemDetails();
-builder.Services.AddApiVersioning();
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(ApplicationAssemblyReference).Assembly));
-builder.Services.AddMappingConfiguration();
-builder.Services.AddMemoryCache();
-
-var app = builder.Build();
-
-app.UseMiddleware<CustomLoggingMiddleware>();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace CSharpApp.Api
 {
-    app.MapOpenApi();
-}
-
-//app.UseHttpsRedirection();
-
-var versionedEndpointRouteBuilder = app.NewVersionedApi();
-
-versionedEndpointRouteBuilder.MapGet("api/v{version:apiVersion}/products", async (IMediator mediator) =>
+    public class Program
     {
-        var products = await mediator.Send(new GetProductsQuery());
-        return products;
-    })
-    .WithName("GetProducts")
-    .HasApiVersion(1.0);
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-versionedEndpointRouteBuilder.MapGet("api/v{version:apiVersion}/categories", async (IMediator mediator) =>
-{
-    var categories = await mediator.Send(new GetCategoriesQuery());
-    return categories;
-})
-    .WithName("GetCategories")
-    .HasApiVersion(1.0);
+            var logger = new LoggerConfiguration().ReadFrom.Configuration(builder.Configuration).CreateLogger();
+            builder.Logging.ClearProviders().AddSerilog(logger);
 
-versionedEndpointRouteBuilder.MapGet("api/v{version:apiVersion}/products/{productId}", async (int productId, IMediator mediator) =>
-{
-    var product = await mediator.Send(new GetProductByIdQuery(productId));
-    return product;
-})
-    .WithName("GetProductById")
-    .HasApiVersion(1.0);
+            // Add services to the container.
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+            builder.Services.AddDefaultConfiguration();
+            builder.Services.AddHttpConfiguration();
+            builder.Services.AddProblemDetails();
+            builder.Services.AddApiVersioning();
+            builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(ApplicationAssemblyReference).Assembly));
+            builder.Services.AddMappingConfiguration();
+            builder.Services.AddMemoryCache();
 
-versionedEndpointRouteBuilder.MapGet("api/v{version:apiVersion}/categories/{categoryId}", async (int categoryId, IMediator mediator) =>
-{
-    var category = await mediator.Send(new GetCategoryByIdQuery(categoryId));
-    return category;
-})
-    .WithName("GetCategoryById")
-    .HasApiVersion(1.0);
+            var app = builder.Build();
 
-versionedEndpointRouteBuilder.MapPost("api/v{version:apiVersion}/products", async (Product product, IMediator mediator) =>
-{
-    var newProduct = await mediator.Send(new CreateProductCommand(product));
-    return newProduct;
-})
-    .WithName("CreateProduct")
-    .HasApiVersion(1.0);
+            app.UseMiddleware<CustomLoggingMiddleware>();
 
-versionedEndpointRouteBuilder.MapPost("api/v{version:apiVersion}/categories", async (Category category, IMediator mediator) =>
-{
-    var newCategory = await mediator.Send(new CreateCategoryCommand(category));
-    return newCategory;
-})
-    .WithName("CreateCategory")
-    .HasApiVersion(1.0);
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
 
-app.Run();
+            //app.UseHttpsRedirection();
+
+            var versionedEndpointRouteBuilder = app.NewVersionedApi();
+
+            versionedEndpointRouteBuilder.MapGet("api/v{version:apiVersion}/products", async (IMediator mediator) =>
+                {
+                    var products = await mediator.Send(new GetProductsQuery());
+                    return products;
+                })
+                .WithName("GetProducts")
+                .HasApiVersion(1.0);
+
+            versionedEndpointRouteBuilder.MapGet("api/v{version:apiVersion}/categories", async (IMediator mediator) =>
+            {
+                var categories = await mediator.Send(new GetCategoriesQuery());
+                return categories;
+            })
+                .WithName("GetCategories")
+                .HasApiVersion(1.0);
+
+            versionedEndpointRouteBuilder.MapGet("api/v{version:apiVersion}/products/{productId}", async (int productId, IMediator mediator) =>
+            {
+                var product = await mediator.Send(new GetProductByIdQuery(productId));
+                return product;
+            })
+                .WithName("GetProductById")
+                .HasApiVersion(1.0);
+
+            versionedEndpointRouteBuilder.MapGet("api/v{version:apiVersion}/categories/{categoryId}", async (int categoryId, IMediator mediator) =>
+            {
+                var category = await mediator.Send(new GetCategoryByIdQuery(categoryId));
+                return category;
+            })
+                .WithName("GetCategoryById")
+                .HasApiVersion(1.0);
+
+            versionedEndpointRouteBuilder.MapPost("api/v{version:apiVersion}/products", async (Product product, IMediator mediator) =>
+            {
+                var newProduct = await mediator.Send(new CreateProductCommand(product));
+                return newProduct;
+            })
+                .WithName("CreateProduct")
+                .HasApiVersion(1.0);
+
+            versionedEndpointRouteBuilder.MapPost("api/v{version:apiVersion}/categories", async (Category category, IMediator mediator) =>
+            {
+                var newCategory = await mediator.Send(new CreateCategoryCommand(category));
+                return newCategory;
+            })
+                .WithName("CreateCategory")
+                .HasApiVersion(1.0);
+
+            app.Run();
+        }
+    }
+}
---
+++ src/CSharpApp.Tests/Endpoints/ProductApiTests.cs
@@ -0,0 +1,125 @@
+﻿using CSharpApp.Application.Commands.Product;
+using CSharpApp.Application.Queries.Product;
+using CSharpApp.Core.Dtos;
+using MediatR;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System.Net.Http.Json;
+
+namespace CSharpApp.Tests.Endpoints
+{
+    public class ProductApiTests : IClassFixture<WebApplicationFactory<Api.Program>>
+    {
+        private readonly Mock<IMediator> _mediatorMock;
+        private readonly HttpClient _client;
+
+        public ProductApiTests(WebApplicationFactory<Api.Program> factory)
+        {
+
+            _mediatorMock = new Mock<IMediator>();
+            _client = factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    services.AddSingleton(_mediatorMock.Object);
+                });
+            }).CreateClient();
+        }
+
+        [Fact]
+        public async Task Given_IRequestForAllProducts_When_GetProducts_Then_ReturnsCollectionOfProducts()
+        {
+            // Arrange
+            var productsList = new List<Product>
+            {
+                new() {
+                    Id = 1,
+                    CategoryId = 1,
+                    Description = "Test description",
+                    Price = 100,
+                    Title = "Test title"
+                },
+                new() {
+                    Id = 2,
+                    CategoryId = 1,
+                    Description = "Test description 2",
+                    Price = 50,
+                    Title = "Test title 2"
+                }
+            };
+            _mediatorMock.Setup(m => m.Send(It.IsAny<GetProductsQuery>(), It.IsAny<CancellationToken>())).ReturnsAsync(productsList);
+
+            // Act
+            var response = await _client.GetAsync("api/v1/products");
+
+            // Assert
+            var products = await response.Content.ReadFromJsonAsync<List<Product>>();
+            Assert.True(response.IsSuccessStatusCode);
+            Assert.Equal(2, products.Count);
+            Assert.Equal(1, products.ElementAtOrDefault(0)?.Id);
+            Assert.Equal(1, products.ElementAtOrDefault(0)?.CategoryId);
+            Assert.Equal("Test description", products.ElementAtOrDefault(0)?.Description);
+            Assert.Equal(100, products.ElementAtOrDefault(0)?.Price);
+            Assert.Equal("Test title", products.ElementAtOrDefault(0)?.Title);
+            _mediatorMock.Verify(m => m.Send(It.IsAny<GetProductsQuery>(), It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+
+        [Fact]
+        public async Task Given_IRequestForSpecificProductByItsId_When_GetProductById_Then_ReturnsProduct()
+        {
+            // Arrange
+            var id = 1;
+            var productRecord = new Product
+            {
+                Id = 1,
+                CategoryId = 1,
+                Description = "Test description",
+                Price = 100,
+                Title = "Test title"
+            };
+            _mediatorMock.Setup(m => m.Send(It.IsAny<GetProductByIdQuery>(), It.IsAny<CancellationToken>())).ReturnsAsync(productRecord);
+
+            // Act
+            var response = await _client.GetAsync($"api/v1/products/{id}");
+            var product = await response.Content.ReadFromJsonAsync<Product>();
+
+            // Assert
+            Assert.True(response.IsSuccessStatusCode);
+            Assert.Equal(1, product?.Id);
+            Assert.Equal(1, product?.CategoryId);
+            Assert.Equal("Test description", product?.Description);
+            Assert.Equal(100, product?.Price);
+            Assert.Equal("Test title", product?.Title);
+            _mediatorMock.Verify(m => m.Send(It.IsAny<GetProductByIdQuery>(), It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task Given_IRequestToCreateNewProduct_When_CreateProduct_Then_ReturnsNewProduct()
+        {
+            // Arrange
+            var product = new Product
+            {
+                Id = 1,
+                CategoryId = 1,
+                Description = "Test description",
+                Price = 100,
+                Title = "Test title",
+            };
+            _mediatorMock.Setup(m => m.Send(It.IsAny<CreateProductCommand>(), It.IsAny<CancellationToken>())).ReturnsAsync(product);
+
+            // Act
+            var response = await _client.PostAsJsonAsync($"api/v1/products", product);
+            var newProduct = await response.Content.ReadFromJsonAsync<Product>();
+
+            // Assert
+            Assert.Equal(1, newProduct?.Id);
+            Assert.Equal(1, newProduct?.CategoryId);
+            Assert.Equal("Test description", newProduct?.Description);
+            Assert.Equal(100, newProduct?.Price);
+            Assert.Equal("Test title", newProduct?.Title);
+            _mediatorMock.Verify(m => m.Send(It.IsAny<CreateProductCommand>(), It.IsAny<CancellationToken>()), Times.Once);
+        }
+    }
+}
