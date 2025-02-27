9c2fa8a32ebd5ad996381e2db96db333d16ab550
Sotos Zarpas <sotiriszarpas@hotmail.gr>
2025-02-28 00:41:07 +0100

    Added policies for retrying, breaking and handling timeouts for the calls to the external service

--- src/CSharpApp.Infrastructure/Configuration/DefaultConfiguration.cs
+++ src/CSharpApp.Infrastructure/Configuration/DefaultConfiguration.cs
@@ -1,4 +1,6 @@
 using CSharpApp.Infrastructure.ExternalServices;
+using Polly.Extensions.Http;
+using Polly;
 
 namespace CSharpApp.Infrastructure.Configuration;
 
@@ -12,20 +14,40 @@ public static class DefaultConfiguration
         services.Configure<RestApiSettings>(configuration!.GetSection(nameof(RestApiSettings)));
         services.Configure<HttpClientSettings>(configuration.GetSection(nameof(HttpClientSettings)));
 
+        int.TryParse(configuration["HttpClientSettings:RetryCount"], out int retryCount);
+        int.TryParse(configuration["HttpClientSettings:SleepDuration"], out int sleepDuration);
+        int.TryParse(configuration["HttpClientSettings:LifeTime"], out int lifeTime);
+
+        var retryPolicy = HttpPolicyExtensions.HandleTransientHttpError()
+                                              .WaitAndRetryAsync(retryCount, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+
+        var circuitBreakerPolicy = HttpPolicyExtensions.HandleTransientHttpError()
+                                              .CircuitBreakerAsync(retryCount, TimeSpan.FromSeconds(sleepDuration));
+
+        var timeoutPolicy = Policy.TimeoutAsync<HttpResponseMessage>(TimeSpan.FromSeconds(lifeTime));
+
         services.AddHttpClient<IJwtTokenService, JwtTokenService>(client =>
         {
             client.BaseAddress = new Uri(configuration["RestApiSettings:BaseUrl"]!);
-        });
+        }).AddPolicyHandler(retryPolicy)
+          .AddPolicyHandler(circuitBreakerPolicy)
+          .AddPolicyHandler(timeoutPolicy);
 
         services.AddHttpClient<IProductsService, ProductsService>(client =>
         {
             client.BaseAddress = new Uri(configuration["RestApiSettings:BaseUrl"]!);
-        }).AddHttpMessageHandler<JwtAuthHandler>();
+        }).AddHttpMessageHandler<JwtAuthHandler>()
+          .AddPolicyHandler(retryPolicy)
+          .AddPolicyHandler(circuitBreakerPolicy)
+          .AddPolicyHandler(timeoutPolicy);
 
         services.AddHttpClient<ICategoriesService, CategoriesService>(client =>
         {
             client.BaseAddress = new Uri(configuration["RestApiSettings:BaseUrl"]!);
-        }).AddHttpMessageHandler<JwtAuthHandler>();
+        }).AddHttpMessageHandler<JwtAuthHandler>()
+          .AddPolicyHandler(retryPolicy)
+          .AddPolicyHandler(circuitBreakerPolicy)
+          .AddPolicyHandler(timeoutPolicy);
 
         services.AddTransient<JwtAuthHandler>();
 
